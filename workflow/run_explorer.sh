06b0999566dade24a34e6110223fb04cac189963
Horia Coman <horia141@gmail.com>
2015-03-11 21:16:40 +0000

    Updated the script for launching the explorer service.

--- workflow/run_explorer.sh
+++ workflow/run_explorer.sh
@@ -3,5 +3,17 @@
 # Setup environment variables.
 source ./workflow/setup_env.sh
 
-# Run the crawling server.
+# Run the fetcher service.
+python ./fetcher/__init__.py &
+FETCHER_PID="$!"
+
+# If the application exist, all these tasks must be killed as well.
+trap "kill $FETCHER_PID" EXIT INT
+
+# Run the crawling service. This will run forever, while the others are in the
+# background.
 python ./explorer/server.py
+
+# Kill all tasks if the last one somehow stops.
+echo 'Killing all started jobs'
+kill $FETCHER_PID
