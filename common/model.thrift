c6638bef0686301505ddfd3f9acb84b34046a951
Horia Coman <horia141@gmail.com>
2015-04-11 17:43:55 +0100

    Small changes.

--- common/model.thrift
+++ common/model.thrift
@@ -3,37 +3,13 @@
 namespace py common.model
 namespace java com.zigzag.common.model
 
-enum GenerationStatus {
-    IN_PROGRESS = 1,
-    CLOSED = 2
-}
-
-// Next id: 4
-struct Generation {
-  1: required GenerationStatus status;
-  2: required i32 date_started_ts;
-  3: optional i32 date_ended_ts;
-}
-
-// Next id: 4
-struct ArtifactSource {
-  1: required string name;
-  2: required string start_page_uri;
-  3: optional set<string> subdomains;
-}
-
-// Next id: 3
-struct ScreenConfig {
-  1: required string name;
-  2: required i32 width;
-}
-
 // Next id: 4
 struct TileData {
   1: required i32 width;
   2: required i32 height;
   3: required string uri_path;
 }
+
 // Next id: 4
 struct ImagePhotoData {
   1: required TileData full_image_desc;
@@ -55,8 +31,23 @@ struct PhotoDescription {
   2: optional string description;
   3: required string source_uri;
   4: required string original_uri_path;
-  5: optional ImagePhotoData image_data;
-  6: optional VideoPhotoData video_data;
+  5: optional map<i64, ImagePhotoData> image_data;
+  6: optional map<i64, VideoPhotoData> video_data;
+}
+
+// Next id: 5
+struct ArtifactSource {
+  1: required i64 id;
+  2: required string name;
+  3: required string start_page_uri;
+  4: optional set<string> subdomains;
+}
+
+// Next id: 4
+struct ScreenConfig {
+  1: required i64 id;
+  2: required string name;
+  3: required i32 width;
 }
 
 // Next id: 4
@@ -66,19 +57,16 @@ struct Artifact {
   3: required list<PhotoDescription> photo_descriptions;
 }
 
+enum GenerationStatus {
+    IN_PROGRESS = 1,
+    CLOSED = 2
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Next id: 4
+struct Generation {
+  1: required i64 id;
+  2: required GenerationStatus status;
+  3: required i32 date_started_ts;
+  4: optional i32 date_ended_ts;
+  5: optional list<Artifact> artifacts;
+}
