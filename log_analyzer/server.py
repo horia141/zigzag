f04b6445d55c141509791a865cf9984f7e19cb29
Horia Coman <horia141@gmail.com>
2015-04-21 22:09:50 +0100

    Made the log analysis server work on lighttpd logs, for both api and res serving and accept flags.

--- log_analyzer/server.py
+++ log_analyzer/server.py
@@ -1,48 +1,70 @@
-import re
-import os
+import argparse
+import datetime
 import logging
-from datetime import datetime
+import os
+import re
 
-class ResourceServingLog(object):
-	def __init__(self, log_path, resources_path):
-		self._log_path = log_path
-		self._resources_path = resources_path
+import utils.pidfile as pidfile
 
-	def _load_content(self):
-		f = open(self._log_path, 'r')
-		return f.read()
 
-	def _requested_resources_for_today(self, content):
-		matches = re.findall(r'\d+[.]\d+[.]\d+[.]\d+ - - [[](\d\d/\w+/\d\d\d\d) \d\d:\d\d:\d\d[]] ["]GET /(.+) HTTP/1.1["] 200 -', content)
+class ServingLog(object):
+    def __init__(self, log_path):
+        self._log_path = log_path
+            
+    def size_of_requested_resources_for_today(self):
+        today = datetime.date.today()
+        log_file = open(self._log_path, 'r')
 
-		requested_resources = []
-		today = datetime.today()
-		for date, filename in matches:
-			parsedDate = datetime.strptime(date, '%d/%b/%Y')
-			if parsedDate.day != today.day or parsedDate.month != today.month or parsedDate.year != today.year:
-				continue
-			else:
-				requested_resources.append(filename)
-		
-		return requested_resources
-		
-	def size_of_requested_resources_for_today(self):
-		content = self._load_content()
-		requested_resources = self._requested_resources_for_today(content)
-		size = 0
+        lines_seen = 0
+        lines_processed = 0
+        total_bytes_sent = 0
 
-		for resource in requested_resources:
-			resource_path = os.path.join(self._resources_path, resource)
-			try:
-				size += os.stat(resource_path).st_size
-			except OSError, e:
-				logging.error('Resource %s not found' % resource_path)
+        for line in log_file:
+            lines_seen += 1
+            match = re.match(r'.*[[](.+) [+]\d+[]] ["]GET /.+ HTTP/\d[.]\d["] 200 (\d+)', line)
 
-		return (datetime.today().strftime('%d/%m/%Y %H:%M'), size)
+            if match is None:
+                continue
+
+            try:
+                line_date = datetime.datetime.strptime(match.group(1), '%d/%b/%Y:%H:%M:%S')
+                bytes_sent = int(match.group(2), 10)
+            except ValueError as e:
+                logging.error('Could not parse data from line "%s"', line[0:40])
+                continue
+
+            if line_date.date() != today:
+                logging.info('Here')
+                continue
+
+            total_bytes_sent += bytes_sent
+            lines_processed += 1
+
+        log_file.close()
+
+        return total_bytes_sent
+
 
 def main():
-	servingLog = ResourceServingLog("../var/res_serving.log", "../var/photos")
-	print servingLog.size_of_requested_resources_for_today()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--api_serving_log_path', type=str, required=True,
+        help='Path to the API serving log')
+    parser.add_argument('--res_serving_log_path', type=str, required=True,
+        help='Path to the res serving log')
+    parser.add_argument('--log_path', type=str, required=True,
+        help='Path to the log file')
+    parser.add_argument('--pidfile_path', type=str, required=True,
+        help='Path for the pidfile')
+    args = parser.parse_args()
+
+    pidfile.write_pidfile(args.pidfile_path)
+
+    logging.basicConfig(level=logging.INFO, filename=args.log_path)
+
+    api_serving_log = ServingLog(args.api_serving_log_path)
+    print api_serving_log.size_of_requested_resources_for_today()
+    res_serving_log = ServingLog(args.res_serving_log_path)
+    print res_serving_log.size_of_requested_resources_for_today()
 
 
 if __name__ == '__main__':
