972e803c84e0b9bafc40aeac8ebc76589132202b
Horia Coman <horia141@gmail.com>
2015-05-09 14:36:28 +0100

    Added generation of users to the application recipe.

--- workflow/cookbooks/application.rb
+++ workflow/cookbooks/application.rb
@@ -1,34 +1,56 @@
-directory "var" do
+# Define groups and users used by different components.
+group 'zigzag'
+
+user 'zigzag-api-serving' do
+  comment 'User for the API serving component'
+  group 'zigzag'
+  shell '/bin/bash'
+end
+
+user 'zigzag-res-serving' do
+  comment 'User for the resources serving component'
+  group 'zigzag'
+  shell '/bin/bash'
+end
+
+# Define directory structure for the runtime data.
+directory 'var' do
   owner 'horia'
   group 'horia'
   mode '0755'
   action :create
 end
 
-directory "var/db_backup" do
+directory 'var/db_backup' do
   owner 'horia'
   group 'horia'
   mode '0755'
   action :create
 end
 
-directory "var/photos" do
+directory 'var/photos' do
   owner 'horia'
   group 'horia'
   mode '0755'
   action :create
 end
 
-directory "var/photos/original" do
+directory 'var/photos/original' do
   owner 'horia'
   group 'horia'
   mode '0755'
   action :create
 end
 
-directory "var/photos/processed" do
+directory 'var/photos/processed' do
   owner 'horia'
   group 'horia'
   mode '0755'
   action :create
 end
+
+# Setup serving.
+
+# Setup API serving.
+
+# Setup resources serving.
