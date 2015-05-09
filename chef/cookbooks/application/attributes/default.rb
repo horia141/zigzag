1ffe65296d2a1d913bb28d1c4bf8597afe4a38a3
Horia Coman <horia141@gmail.com>
2015-05-09 21:22:24 +0100

    Added the configuration for the resources server. A lotta work.

--- chef/cookbooks/application/attributes/default.rb
+++ chef/cookbooks/application/attributes/default.rb
@@ -1,1 +1,24 @@
-default['application']['work_dir'] = '/home/horiacoman/Work/zigzag'
+default['application']['user'] = 'zigzag'
+default['application']['group'] = 'zigzag'
+default['application']['work_dir'] = '/zigzag'
+default['application']['db_backup_dir'] = File.join(default['application']['work_dir'], 'db_backup')
+default['application']['config_dir'] = File.join(default['application']['work_dir'], 'config')
+default['application']['photos_dir'] = File.join(default['application']['work_dir'], 'photos')
+default['application']['original_photos_dir'] = File.join(default['application']['photos_dir'], 'original')
+default['application']['processed_photos_dir'] = File.join(default['application']['photos_dir'], 'processed')
+
+default['application']['api_serving']['user'] = 'zigzag_api_serving'
+
+default['application']['res_serving']['name'] = 'zigzag_res_serving'
+default['application']['res_serving']['config'] = File.join(default['application']['config_dir'], 'res_serving.lighttpd')
+default['application']['res_serving']['daemon_script'] = '/etc/init.d/zigzag_res_serving'
+default['application']['res_serving']['daemon_pid_file'] = File.join(default['application']['work_dir'], 'res_serving.daemon.pid')
+default['application']['res_serving']['daemon_error_log'] = File.join(default['application']['work_dir'], 'res_serving.daemon.error.log')
+default['application']['res_serving']['daemon_debug_log'] = File.join(default['application']['work_dir'], 'res_serving.daemon.debug.log')
+default['application']['res_serving']['user'] = 'zigzag_res_serving'
+default['application']['res_serving']['port'] = 9001
+default['application']['res_serving']['pid_file'] = File.join(default['application']['work_dir'], 'res_serving.lighttpd.pid')
+default['application']['res_serving']['error_log'] = File.join(default['application']['work_dir'], 'res_serving.lighttpd.error.log')
+default['application']['res_serving']['access_log'] = File.join(default['application']['work_dir'], 'res_serving.lighttpd.access.log')
+default['application']['res_serving']['allowed_extension'] = 'jpg|mp4'
+default['application']['res_serving']['expire'] = 'access plus 1 months'
--- chef/cookbooks/application/recipes/default.rb
+++ chef/cookbooks/application/recipes/default.rb
@@ -1,54 +1,71 @@
 # Keep the APT cache up-to-date.
 include_recipe 'apt::default'
 
-# Define groups and users used by different components.
-group 'zigzag'
+# Setup the proper packages.
+package 'daemon'
+package 'lighttpd'
 
-user 'zigzag-api-serving' do
-  comment 'User for the API serving component'
-  group 'zigzag'
+# Define groups and users used by different components.
+group node.default['application']['group']
+
+user node.default['application']['user'] do
+  comment 'Master user for the application'
+  group node.default['application']['group']
   shell '/bin/bash'
 end
 
-user 'zigzag-res-serving' do
+user node.default['application']['api_serving']['user'] do
+  comment 'User for the API serving component'
+  group node.default['application']['group']
+  shell '/bin/bash'
+end
+
+user node.default['application']['res_serving']['user'] do
   comment 'User for the resources serving component'
-  group 'zigzag'
+  group node.default['application']['group']
   shell '/bin/bash'
 end
 
 # Define directory structure for the runtime data.
-directory File.join(node.default['application']['work_dir'], 'var') do
-  owner 'horia'
-  group 'zigzag'
-  mode '0755'
+directory node.default['application']['work_dir'] do
+  owner node.default['application']['user']
+  group node.default['application']['group']
+  mode '0770'
   action :create
 end
 
-directory File.join(node.default['application']['work_dir'], 'var/db_backup') do
-  owner 'horia'
-  group 'zigzag'
-  mode '0755'
+directory node.default['application']['db_backup_dir'] do
+  owner node.default['application']['user']
+  group node.default['application']['group']
+  mode '0770'
   action :create
 end
 
-directory File.join(node.default['application']['work_dir'], 'var/photos') do
-  owner 'horia'
-  group 'zigzag'
-  mode '0755'
+directory node.default['application']['config_dir'] do
+  owner node.default['application']['user']
+  group node.default['application']['group']
+  mode '0770'
   action :create
 end
 
-directory File.join(node.default['application']['work_dir'], 'var/photos/original') do
-  owner 'horia'
-  group 'zigzag'
-  mode '0755'
+directory node.default['application']['photos_dir'] do
+  owner node.default['application']['user']
+  group node.default['application']['group']
+  mode '0770'
   action :create
 end
 
-directory File.join(node.default['application']['work_dir'], 'var/photos/processed') do
-  owner 'horia'
-  group 'zigzag'
-  mode '0755'
+directory node.default['application']['original_photos_dir'] do
+  owner node.default['application']['user']
+  group node.default['application']['group']
+  mode '0770'
+  action :create
+end
+
+directory node.default['application']['processed_photos_dir'] do
+  owner node.default['application']['user']
+  group node.default['application']['group']
+  mode '0770'
   action :create
 end
 
@@ -57,3 +74,26 @@ end
 # Setup API serving.
 
 # Setup resources serving.
+
+template node.default['application']['res_serving']['config'] do
+  source 'res_serving.lighttpd.erb'
+  owner node.default['application']['res_serving']['user']
+  group node.default['application']['group']
+  mode '0440'
+  verify "lighttpd -t -f %{file}"
+end
+
+template node.default['application']['res_serving']['daemon_script'] do
+  source 'res_serving.daemon.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+service node.default['application']['res_serving']['name'] do
+  init_command node.default['application']['res_serving']['daemon_script']
+  supports :start => true, :stop => true, :restart => true, :status => true
+  action [:enable, :start]
+  subscribes :restart, "template[#{node.default['application']['res_serving']['daemon_script']}]", :delayed
+  subscribes :restart, "template[#{node.default['application']['res_serving']['config']}]", :delayed
+end
