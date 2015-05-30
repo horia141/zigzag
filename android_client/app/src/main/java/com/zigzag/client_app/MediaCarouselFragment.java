201619c3c56e2c5b7088b9d8c426bced03bc0ba9
Horia Coman <horia141@gmail.com>
2015-05-30 15:15:13 +0100

    Cleaned up the code of the MediaCarouselFragment a bit.

--- android_client/app/src/main/java/com/zigzag/client_app/MediaCarouselFragment.java
+++ android_client/app/src/main/java/com/zigzag/client_app/MediaCarouselFragment.java
@@ -1,57 +1,47 @@
 package com.zigzag.client_app;
 
 import android.app.Fragment;
-import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.zigzag.client_app.controller.Controller;
 import com.zigzag.client_app.ui.ArtifactView;
-import com.zigzag.client_app.ui.PhotoDescriptionView;
 import com.zigzag.common.model.Artifact;
 import com.zigzag.common.model.ArtifactSource;
 import com.zigzag.common.model.ImagePhotoData;
 import com.zigzag.common.model.PhotoData;
 import com.zigzag.common.model.PhotoDescription;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-public class MediaCarouselFragment extends Fragment implements Controller.ArtifactResourcesListener {
+public class MediaCarouselFragment extends Fragment
+        implements Controller.ArtifactResourcesListener {
 
     @Nullable private Artifact artifact;
-    @Nullable private ArtifactSource artifactSource;
-    @Nullable private Date dateAdded;
 
     public MediaCarouselFragment() {
         artifact = null;
-        artifactSource = null;
-        dateAdded = null;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        Log.i("ZigZag/MediaCarouselFragment", "Creating view");
+            Bundle savedInstanceState) {
+        Log.i("ZigZag/MediaCarousel", "Creating view");
 
+        // Next section assumes that (1) args contains "artifact_id", which is a String representing
+        // an artifact URI, and (2) that said URI exists in the system.
         Bundle args = getArguments();
         String artifactPageUri = args.getString("artifact_id");
         artifact = Controller.getInstance(getActivity()).getArtifactByPageUri(artifactPageUri);
-        artifactSource = Controller.getInstance(getActivity()).getSourceForArtifact(artifact);
-        dateAdded = Controller.getInstance(getActivity()).getDateForArtifact(artifact);
+        ArtifactSource artifactSource = Controller.getInstance(getActivity())
+                .getSourceForArtifact(artifact);
+        Date dateAdded = Controller.getInstance(getActivity()).getDateForArtifact(artifact);
 
         final ArtifactView rootView = (ArtifactView) inflater.inflate(
                 R.layout.fragment_media_carousel, container, false);
@@ -63,12 +53,14 @@ public class MediaCarouselFragment extends Fragment implements Controller.Artifa
     @Override
     public void onStart() {
         super.onStart();
+        Log.i("ZigZag/MediaCarousel", "Starting");
         Controller.getInstance(getActivity()).fetchArtifactResources(artifact, this);
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        Log.i("ZigZag/MediaCarousel", "Stopping");
         Controller.getInstance(getActivity()).deregisterArtifactResources(artifact, this);
     }
 
@@ -78,20 +70,19 @@ public class MediaCarouselFragment extends Fragment implements Controller.Artifa
     }
 
     @Override
-    public void onResourcesForArtifact(Artifact artifact, int photoDescriptionIdx, int tileOrFrameIdx, Bitmap image) {
-        Log.i("ZigZag", String.format("Got %d/%d", photoDescriptionIdx, tileOrFrameIdx));
+    public void onResourcesForArtifact(Artifact artifact, int photoDescriptionIdx,
+            int tileOrFrameIdx, Bitmap image) {
+        Log.i("ZigZag/MediaCarousel", String.format("ResourcesForArtifact for photo %d and tile %d",
+                photoDescriptionIdx, tileOrFrameIdx));
 
         if (artifact != this.artifact) {
-            Log.e("ZigZag/MediaCarouselFragment", "Got out of sync image update");
+            Log.e("ZigZag/MediaCarousel", "Got out of sync photo update");
             return;
         }
 
-        if (photoDescriptionIdx < 0) {
-            Log.e("ZigZag/MediaCarouselFragment", String.format("Got a negative image index %d", photoDescriptionIdx));
-            return;
-        } else if (photoDescriptionIdx >= artifact.getPhoto_descriptionsSize()) {
-            Log.e("ZigZag/MediaCarouselFragment", String.format("Got an image for list index %d, but list" +
-                    " has size %d", photoDescriptionIdx, artifact.getPhoto_descriptionsSize()));
+        if (photoDescriptionIdx < 0 || photoDescriptionIdx >= artifact.getPhoto_descriptionsSize()) {
+            Log.e("ZigZag/MediaCarousel", String.format("Out of bounds photo at %d, but only"
+                    + " %d photos", photoDescriptionIdx, artifact.getPhoto_descriptionsSize()));
             return;
         }
 
@@ -100,21 +91,22 @@ public class MediaCarouselFragment extends Fragment implements Controller.Artifa
         PhotoData photoData = photoDescription.getPhoto_data();
 
         if (photoData.isSetToo_big_photo_data()) {
-            Log.e("ZigZag/MediaCarouselFragment", "Received image data for a very large image");
+            Log.e("ZigZag/MediaCarousel", "Received image data for a very large image");
         } else if (photoData.isSetImage_photo_data()){
-            if (tileOrFrameIdx < 0) {
-                Log.e("ZigZag/MediaCarouselFragment", String.format("Got a negative tile index %d", tileOrFrameIdx));
-                return;
-            } else if (tileOrFrameIdx >= photoDescription.getPhoto_data().getImage_photo_data().getTilesSize()) {
-                Log.e("ZigZag/MediaCarouselFragment", String.format("Out of bounds tile index with tile %d but it" +
-                        " has size %d", tileOrFrameIdx, photoData.getImage_photo_data().getTilesSize()));
+            ImagePhotoData imagePhotoData = photoDescription.getPhoto_data().getImage_photo_data();
+
+            if (tileOrFrameIdx < 0 || tileOrFrameIdx >= imagePhotoData.getTilesSize()) {
+                Log.e("ZigZag/MediaCarousel", String.format("Out of bounds tile index at %d,"
+                        + " but only %d tiles, for photo %d", tileOrFrameIdx,
+                        imagePhotoData.getTilesSize(), photoDescriptionIdx));
                 return;
             }
 
             artifactView.setBitmapForTile(photoDescriptionIdx, tileOrFrameIdx, image);
         } else if (photoData.isSetVideo_photo_data()) {
             if (tileOrFrameIdx != 0) {
-                Log.e("ZigZag/MediaCarouselFragment", String.format("Got a non-zero tile index %d", tileOrFrameIdx));
+                Log.e("ZigZag/MediaCarousel", String.format("Non-zero tile index %d,"
+                        + " for photo %d", tileOrFrameIdx, photoDescriptionIdx));
                 return;
             }
 
@@ -123,20 +115,19 @@ public class MediaCarouselFragment extends Fragment implements Controller.Artifa
     }
 
     @Override
-    public void onVideoResourcesForArtifact(Artifact artifact, int photoDescriptionIdx, String localPathToVideo) {
-        Log.i("ZigZag", String.format("Got Video %d", photoDescriptionIdx));
+    public void onVideoResourcesForArtifact(Artifact artifact, int photoDescriptionIdx,
+            String localPathToVideo) {
+        Log.i("ZigZag/MediaCarousel", String.format("VideoResourcesForArtifact for photo %d",
+                photoDescriptionIdx));
 
         if (artifact != this.artifact) {
-            Log.e("ZigZag/MediaCarouselFragment", "Got out of sync video update");
+            Log.e("ZigZag/MediaCarousel", "Got out of sync photo update");
             return;
         }
 
-        if (photoDescriptionIdx < 0) {
-            Log.e("ZigZag/MediaCarouselFragment", String.format("Got a negative image index %d", photoDescriptionIdx));
-            return;
-        } else if (photoDescriptionIdx >= artifact.getPhoto_descriptionsSize()) {
-            Log.e("ZigZag/MediaCarouselFragment", String.format("Got an image for list index %d, but list" +
-                    " has size %d", photoDescriptionIdx, artifact.getPhoto_descriptionsSize()));
+        if (photoDescriptionIdx < 0 || photoDescriptionIdx >= artifact.getPhoto_descriptionsSize()) {
+            Log.e("ZigZag/MediaCarousel", String.format("Out of bounds photo at %d, but only"
+                    + " %d photos", photoDescriptionIdx, artifact.getPhoto_descriptionsSize()));
             return;
         }
 
@@ -145,7 +136,7 @@ public class MediaCarouselFragment extends Fragment implements Controller.Artifa
         PhotoData photoData = photoDescription.getPhoto_data();
 
         if (!photoData.isSetVideo_photo_data()) {
-            Log.e("ZigZag/MediaCarouselFragment", "Received video update for something which is not a video");
+            Log.e("ZigZag/MediaCarousel", "Received video update for something which is not a video");
             return;
         }
 
@@ -154,7 +145,7 @@ public class MediaCarouselFragment extends Fragment implements Controller.Artifa
 
     @Override
     public void onError(String errorDescription) {
-        Log.i("ZigZag", String.format("Error %s", errorDescription));
+        Log.i("ZigZag/MediaCarousel", String.format("Error %s", errorDescription));
     }
 
     public static MediaCarouselFragment newInstance(Artifact artifact) {
