0a98a8836f7e217cf040dd863de10c7a8a77214f
Horia Coman <horia141@gmail.com>
2015-07-16 23:57:43 +0100

    Added generation of Android graphics assets.

--- workflow/graphics_gen.py
+++ workflow/graphics_gen.py
@@ -1,5 +1,9 @@
 import argparse
+import os
+import os.path
+import shutil
 
+from PIL import Image
 import yaml
 
 
@@ -22,12 +26,47 @@ class GraphicsGenerator(object):
 
 
 class AndroidGenerator(GraphicsGenerator):
+    class Profile(object):
+        def __init__(self, out_path, target_width):
+            self.out_path = out_path
+            self.target_width = target_width
+
+    _PROFILES = {
+        'none': Profile('drawable', None),
+        'mdpi': Profile('drawable-mdpi', 48),
+        'hdpi': Profile('drawable-hdpi', 72),
+        'xhdpi': Profile('drawable-xhdpi', 96),
+        'xxhdpi': Profile('drawable-xxhdpi', 144)
+    }
+
     def __init__(self, output_dir_path):
         super(AndroidGenerator, self).__init__()
         self._output_dir_path = output_dir_path
 
+    def __enter__(self):
+        if not os.path.exists(self._output_dir_path):
+            os.mkdir(self._output_dir_path)
+        for p_name, profile in self._PROFILES.iteritems():
+            profile_path = os.path.join(self._output_dir_path, profile.out_path)
+            if os.path.exists(profile_path):
+                if os.path.isfile(profile_path):
+                    os.remove(profile_path)
+                else:
+                    shutil.rmtree(profile_path)
+            os.mkdir(profile_path)
+        return self
+
     def generate(self, g_name, g_config):
-        print g_name
+        for p_name, profile in self._PROFILES.iteritems():
+            path = g_config['path']
+            out_path = os.path.join(self._output_dir_path, profile.out_path, g_name)
+
+            if g_config['action'] == 'copy':
+                shutil.copy2(path, out_path)
+            elif profile.target_width is not None:
+                image = Image.open(path)
+                image_resized = _resize_to_width(image, profile.target_width)
+                image_resized[0].save(out_path)
 
 
 class IOSGenerator(GraphicsGenerator):
@@ -95,10 +134,21 @@ def _load_config(config_path):
             if g_config['action'] not in ACTIONS:
                 raise Exception('Invalid action for "%s' % g_name)
 
+            g_config['path'] = os.path.join(os.path.dirname(config_path), g_name)
+
             for p in platforms:
                 config_for_platform[p][g_name] = g_config
 
     return config_for_platform
+
+
+def _resize_to_width(image, desired_width):
+    (width, height) = image.size
+    aspect_ratio = float(height) / float(width)
+    desired_height = int(aspect_ratio * desired_width)
+    desired_height = desired_height + desired_height % 2 # Always a multiple of 2.
+    image_resized = image.resize((desired_width, desired_height), Image.ANTIALIAS)
+    return (image_resized, desired_height)
 
 
 if __name__ == '__main__':
